42e1eafe9513556cde255ad23dd27bc7071e6336
Betha Hema Manikanta <hemamanikanta345@gmail.com>
2025-09-23 23:39:20 +0530

    adding from remote branch

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,12 +1,9 @@
 import { test, expect } from '@playwright/test';
 test.describe('basic login tests', () =>{
-    
+
 test.only('basic login test', async ({ page }) => {
-  await page.goto('https://demoqa.com/login');
-  await page.fill('#userName', 'testuser');
-  await page.fill('#password', 'Test@123');
-  await page.click('#login');
-  await expect(page.locator('#userName-value')).toHaveText('testuser');
+  await page.goto('https://demoqa.com/');
+  await page.click('#app > div > div > div.home-body > div > div:nth-child(1) > div');
 });
 });
 
