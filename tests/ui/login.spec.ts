0f06a8f800506ef59ef76d31fcef241eab24b80d
Betha Hema Manikanta <hemamanikanta345@gmail.com>
2025-09-24 19:35:43 +0530

    setup github actiosn

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 test.describe('basic login tests', () =>{
 
-test.only('basic login test', async ({ page }) => {
+test('basic login test', async ({ page }) => {
   //await page.goto('https://demoqa.com/');
   await page.goto('https://demoqa.com/', { timeout: 60000, waitUntil: 'networkidle' });
   await page.click('#app > div > div > div.home-body > div > div:nth-child(1) > div');
