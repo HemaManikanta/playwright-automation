c3624114ea5bae444f5a8b693da61e138524d4f6
Betha Hema Manikanta <hemamanikanta345@gmail.com>
2025-09-24 19:27:53 +0530

    setup github actiosn

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+   // Maximum time a test can run (ms)
+  timeout: 60000, // 60 seconds instead of default 30 seconds
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 test.describe('basic login tests', () =>{
 
 test.only('basic login test', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
+  //await page.goto('https://demoqa.com/');
+  await page.goto('https://demoqa.com/', { timeout: 60000, waitUntil: 'networkidle' });
   await page.click('#app > div > div > div.home-body > div > div:nth-child(1) > div');
 });
 });
